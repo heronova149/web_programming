f608184075eb158e48e6f4c04ece40f83c21162a
Nahid Kassam <nahidou2001@gmail.com>
2025-10-08 14:19:50 +0200

    API connection

--- index.js
+++ index.js
@@ -6,6 +6,25 @@ app.get('/', (req, res) => {
     res.send('Hello World');
 })
 
+const goats = [
+    {
+        "id": 1,
+        "name":"t-rex"
+    },
+    {
+        "id": 2,
+        "name":"connie"
+    },
+    {
+        "id": 2,
+        "name":"clyde"
+    },
+    ];
+
+app.get('/test', (req, res) => {
+    res.json(goats);
+})
+
 console.log("... SERVER IS RUNNING ...");
 
 app.listen(3000);
