f60c27f2ab5a4bf57e7ff64ea5da71ccb9e5b2ff
Nahid Kassam <nahidou2001@gmail.com>
2025-11-12 14:32:53 +0100

    completed API + MYSQL DB

--- routes/rankings.js
+++ routes/rankings.js
@@ -1,8 +1,23 @@
 const express = require("express");
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    res.send("[GET] show all the rankings");
+const { PrismaClient } = require('../generated/prisma');
+const prisma = new PrismaClient();
+
+router.get('/', async(req, res) => {
+    const ranking = await prisma.$queryRaw
+    `SELECT
+        song_id, S.name as songname, a.name asartistname, SUM(Points)
+    FROM
+        Votes as V
+            INNER JOIN songs AS S USING(song_id)
+            INNER JOIN artists as A USING(artist_id) 
+    GROUP BY
+        s.song_id
+    ORDER BY
+        SUM(Points) DESC;`
+    
+    res.send(ranking);
 })
 
 module.exports = router;
